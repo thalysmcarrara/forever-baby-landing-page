2139baad21e9ec45df320ad0f093ac6d828695c5
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-03-05 01:07:04 -0400

    :sparkles: feat: header desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Header } from './components';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
