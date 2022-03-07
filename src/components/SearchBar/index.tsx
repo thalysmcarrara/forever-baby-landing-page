dde5323f2eade6ee007f3a51f306d3b02fd743b3
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-03-07 07:52:24 -0400

    :bug: fix: searchBar validation

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,34 @@
-import { FormEvent } from 'react';
+import { toast } from 'react-toastify';
 
 import style from './style.module.scss';
 
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+
 export default function SearchBar() {
-  const handleSubmit = (event: FormEvent) => {
+  const isAllLetters = (word: string): boolean => {
+    let isAlphabet = true;
+
+    for (let index = 0; index < word.length; index += 1) {
+      if (/[^a-zA-Z0-9]+/g.test(word[index])) {
+        isAlphabet = false;
+        break;
+      }
+    }
+    return isAlphabet;
+  };
+
+  const handleSubmit = (event: any) => {
     event.preventDefault();
+    const word: string = event.target.search.value;
+    if (word === '') {
+      toast.error('O campo não pode ser vazio');
+    } else if (word.includes(' ')) {
+      toast.error('Não é permitido espaços');
+    } else if (!isAllLetters(word)) {
+      toast.error('Não é permitido caracteres especiais');
+    } else if (word.length < 3) {
+      toast.error('Digite pelo menos 3 caracteres');
+    }
   };
 
   return (
@@ -12,11 +36,7 @@ export default function SearchBar() {
       <input
         type="text"
         name="search"
-        pattern="[a-z\s]+$"
         placeholder="Qual o mimo de hoje para o seu anjinho?"
-        minLength={3}
-        maxLength={40}
-        required
       />
       <button type="submit">BUSCAR</button>
     </form>
